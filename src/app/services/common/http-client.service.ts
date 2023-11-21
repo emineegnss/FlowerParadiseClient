81d6907291d7ede3e1e3e0fc1fe56bad9f30c525
emineegnss <80625362+emineegnss@users.noreply.github.com>
2023-11-21 23:28:42 +0300

    21/11 Commit/1

---
+++ src/app/services/common/http-client.service.ts
@@ -0,0 +1,27 @@
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpClientService {
+  constructor(
+    private httpClient: HttpClient,
+    @Inject('baseUrl') private baseUrl: string
+  ) {}
+  get<T>(requestParameter: Partial<RequestParameters> ) {
+    let url: string = '';
+    
+  }
+  post() {}
+  put() {}
+  delete() {}
+}
+export class RequestParameters{
+  controller?:string;
+  action?:string;
+  headers?:HttpHeaders;
+  baseUrl?:string;
+  fullEndPoint?:string
+
+}
